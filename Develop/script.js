5b93f429759c45ce0494f9234eea8211f9de20a2
boocipher <boocipher@gmail.com>
2022-01-05 23:54:20 -0600

    validation function

--- Develop/script.js
+++ Develop/script.js
@@ -4,20 +4,23 @@ var generateBtn = document.querySelector("#generate");
 // Array of options
 // TODO - add array values
 var lowerOptions = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-// var upperOptions = lowerOptions.toUpperCase();
-// console.log(upperOptions);
-var numOptions = ["0123456789"];
-var charOptions = ["!@#$%^&*()_+~`|}{[]\:;?><,./-="];
+// function below by askBCS
+var upperOptions = lowerOptions.map(change => change.toUpperCase());
+console.log(upperOptions);
+var numOptions = ["0","1","2","3","4","5","6","7","8","9"];
+var charOptions = "!@#$%^&*()_+~`|}{[]\:;?><,./-=".split('');
+console.log(charOptions);
 
 
 //Main function
 function generatePassword() {
   var length = promptPWlength();
   console.log("promptPWlength is:" + length);
-  // lowerCase();
-  // upperCase();
-  // numeric();
-  // spChar();
+  lowerCase();
+  upperCase();
+  numeric();
+  spChar();
+  validate();
 
   var pass = "1234";
   // generate password
@@ -31,11 +34,13 @@ function generatePassword() {
 //show prompt until result is >= 8 or <=128 w/ while loop
 function promptPWlength() {
   var length;
-  // if (!length) {
-  //   return;
-  // }
+  var pwLength = prompt("Please choose a number between 8 and 128 for the password length");
+  length = parseInt(pwLength);
+  if (!pwLength) {
+    return;
+  }
   while (isNaN(length) || length < 8 || length > 128) {
-    var pwLength = prompt("Please choose a number between 8 and 128 for the password length");
+    pwLength = prompt("Please choose a number between 8 and 128 for the password length");
     length = parseInt(pwLength);
     console.log(length);
   } 
@@ -68,10 +73,19 @@ function spChar() {
 
 //Validate if at least one char type is selected (input validation); 
 //if not -return
+function validate() {
+  if (lowerCase || upperCase || numeric || spChar) {
+    console.log("Char selection OK");
+  } else {
+    alert("Choose at least one character type!");
+    generatePassword();
+  }
+}
 
-//for each selected char type, concat in an array
 
-//choose randomly the number of char selected, from the array created based on user choices
+//for each selected char type, concat in a new array
+
+//choose randomly based on the number of char selected, from the array created based on user choices
 
 
 
